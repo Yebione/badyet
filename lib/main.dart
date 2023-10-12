bbed6b8a8b14ead431576b8def5021b0c002c250
Yebione <kyle.billones@wvsu.edu.ph>
2023-10-12 22:04:34 +0800

    Removed "Debug" banner.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(     
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const badyetHome(),
     );
   }
