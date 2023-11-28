ac3c8d7ced89290c0d7b20c07824357fefbb0d97
Yebione <kyle.billones@wvsu.edu.ph>
2023-11-28 17:13:56 +0800

    moved widgets file into a widgets folder

---
+++ lib/widgets/BottomNavbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatelessWidget {
+  const BottomNavbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      //bottom navigation bar on scaffold
+      color: const Color.fromRGBO(233, 233, 233, 1),
+      //shape of notch
+      notchMargin: 5,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.history,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/history');
+                },
+              ),
+            
+            ],
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
