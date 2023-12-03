a7dc56733823ac39afed617da315cb70a22ff722
Yebione <kyle.billones@wvsu.edu.ph>
2023-12-03 20:45:06 +0800

    fixed merge issue

--- lib/widgets/AddButton.dart
+++ lib/widgets/AddButton.dart
@@ -1,50 +1,34 @@
-import 'dart:js_util';
-
 import 'package:flutter/material.dart';
 
 class AddExpenseButton extends StatelessWidget {
   const AddExpenseButton({super.key});
 
   @override
-  State<AddExpenseButton> createState() => _AddExpenseButton();
-}
-
-class _AddExpenseButton extends State<AddExpenseButton> {
-  final addBudgetController = TextEditingController();
-
-  @override
-  void dispose() {
-    addBudgetController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
     return 
-      Container(
-            height: screenWidth/6,
-            width: screenWidth/6,
-            child: FittedBox(
-              child: ClipOval(
-                child: FloatingActionButton(
-                    onPressed: () {},
-                    child: Container(
-                      height: screenWidth/5,
-                      width: screenWidth/5,
-                      child: Center(child: Text("+",  style: GoogleFonts.poppins (fontSize: screenWidth/11, fontWeight: FontWeight.w600, color: Colors.white))),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          gradient: LinearGradient(colors: [
-                            const Color.fromRGBO(53, 122, 213, 1),
-                            const Color.fromRGBO(81, 218, 96, 1)
-                          ])),
-                    )
+      FloatingActionButton(
+        onPressed: () {
+          
+        },
+        backgroundColor: Colors.transparent,
+        shape: CircleBorder(eccentricity: 0.0),
+        child: Container(
 
-                    
-                    ),
-              ),
-            ),
-          );
+          width: 80,
+          height: 80,
+          child: Icon(
+            Icons.add,
+            size: 40,
+            color: Colors.white,
+          ),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(colors: [
+                const Color.fromRGBO(53, 122, 213, 1),
+                const Color.fromRGBO(81, 218, 96, 1)
+              ])),
+        ),
+        //icon inside button
+      );
   }
 }
